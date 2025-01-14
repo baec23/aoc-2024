86eddead1d4cd4cab47cb3ad6fee57dd8685c3de
Changmin Bae <baec23@gmail.com>
2025-01-15 07:30:33 +0900

    day2 part2

--- day2/main.cpp
+++ day2/main.cpp
@@ -4,41 +4,62 @@
 #include <sstream>
 #include <vector>
 
+bool isReportSafe(std::vector<int> &report) {
+  bool isSafe = true;
+  int d = report[1] - report[0];
+  bool isIncreasing = d > 0;
+  int sign;
+  if (isIncreasing) {
+    sign = 1;
+  } else {
+    sign = -1;
+  }
+  for (int i = 1; i < report.size(); i++) {
+    int diff = report[i] - report[i - 1];
+    diff *= sign;
+    // std::cout << "comparing " << reports[i][j - 1] << " and " <<
+    // reports[i][j]
+    //           << " | diff= " << diff << std::endl;
+    if (diff != 1 && diff != 2 && diff != 3) {
+      isSafe = false;
+      break;
+    }
+  }
+  return isSafe;
+}
+
 void part1(std::vector<std::vector<int>> &reports) {
   int numSafeReports = 0;
   for (int i = 0; i < reports.size(); i++) {
-    bool isSafe = true;
-    int d = reports[i][1] - reports[i][0];
-    bool isIncreasing = d > 0;
-    int sign;
-    if (isIncreasing) {
-      sign = 1;
-    } else {
-      sign = -1;
-    }
-    for (int j = 1; j < reports[i].size(); j++) {
-      int diff = reports[i][j] - reports[i][j - 1];
-      diff *= sign;
-      // std::cout << "comparing " << reports[i][j - 1] << " and " <<
-      // reports[i][j]
-      //           << " | diff= " << diff << std::endl;
-      if (diff != 1 && diff != 2 && diff != 3) {
-        isSafe = false;
-        break;
-      }
-    }
-    if (isSafe) {
-      // std::cout << "SAFE" << std::endl << std::endl;
+    if (isReportSafe(reports[i])) {
       numSafeReports++;
-    } else {
-      // std::cout << "UNSAFE" << std::endl << std::endl;
     }
   }
   std::cout << "num safe reports: " << numSafeReports << std::endl;
 }
 
 void part2(std::vector<std::vector<int>> &reports) {
-  // for each report, try removing every level
+  int numSafeReports = 0;
+  for (int i = 0; i < reports.size(); i++) {
+    if (isReportSafe(reports[i])) {
+      numSafeReports++;
+    } else {
+      std::vector<int> trimmedReport;
+      for (int j = 0; j < reports[i].size(); j++) {
+        trimmedReport.clear();
+        for (int k = 0; k < reports[i].size(); k++) {
+          if (k != j) {
+            trimmedReport.push_back(reports[i][k]);
+          }
+        }
+        if (isReportSafe(trimmedReport)) {
+          numSafeReports++;
+          break;
+        }
+      }
+    }
+  }
+  std::cout << "num safe reports: " << numSafeReports << std::endl;
 }
 
 int main() {
@@ -54,7 +75,7 @@ int main() {
       }
       reports.push_back(levels);
     }
-    part1(reports);
+    part2(reports);
 
   } catch (const std::runtime_error &e) {
     std::cerr << e.what() << std::endl;
